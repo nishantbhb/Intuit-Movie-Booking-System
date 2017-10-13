feaad041f15ddd07dc7d4b300140558ed5a16594
Thakur Nishant Singh <nishantbhb@gmail.com>
2017-10-13 18:56:10 +0530

    added profile concept to avoid running run method in main class

--- src/main/java/com/intuit/movie/IntuitMovieBookingSystem/IntuitMovieBookingSystemApplication.java
+++ src/main/java/com/intuit/movie/IntuitMovieBookingSystem/IntuitMovieBookingSystemApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Profile;
 
 @ComponentScan("com.intuit.movie")
 @SpringBootApplication
+@Profile("!test")
 public class IntuitMovieBookingSystemApplication implements CommandLineRunner {
 
 	@Autowired
